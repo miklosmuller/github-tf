38f093fc293bf7d4f834d1e4b9d2bc3e428df49d
Muller, Miklos <miklos.muller@lhsystems.com>
2021-06-21 13:31:46 +0200

    update

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
+variable "token" {
+	type = string
+}
+
 provider "github" {
-#  token = var.token
-  token = var.kiskutya
+  token = var.token
   owner = "miklosmuller"
 }
 
 resource "github_repository" "example-repo" {
-  name        = "example-repo"
+  name        = "example-repo3"
   description = "My new repository for use with Terraform"
 }
